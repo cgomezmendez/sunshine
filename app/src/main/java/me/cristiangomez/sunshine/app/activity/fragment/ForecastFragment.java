07ddeed2da3543cd2bb2e812d116f5bc02a38c27
Cristian Gomez <contact@cristiangomez.me>
2015-06-06 14:48:35 -0400

    added task to sync with weather api

--- app/src/main/java/me/cristiangomez/sunshine/app/activity/fragment/ForecastFragment.java
+++ app/src/main/java/me/cristiangomez/sunshine/app/activity/fragment/ForecastFragment.java
@@ -2,23 +2,31 @@ package me.cristiangomez.sunshine.app.activity.fragment;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import me.cristiangomez.sunshine.R;
+import me.cristiangomez.sunshine.app.activity.net.FetchWeatherTask;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ForecastFragment extends Fragment {
     private ListView mForecastListView;
 
-    public MainActivityFragment() {
+    public ForecastFragment() {
     }
 
     @Override
@@ -27,6 +35,11 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         initializeView(view);
         return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     public void initializeView(View view) {
@@ -42,5 +55,10 @@ public class MainActivityFragment extends Fragment {
                 R.id.list_item_forecast_textview,
                 forecasts);
         mForecastListView.setAdapter(arrayAdapter);
+        requestData();
+    }
+
+    public void requestData() {
+        new FetchWeatherTask().execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
     }
 }
