3bfa3753cacc1917be091fc7eea54fea74fd94b5
Cristian Gomez <contact@cristiangomez.me>
2015-06-06 12:18:33 -0400

    adding some dummy data and binding adapter to listview

--- app/src/main/java/me/cristiangomez/sunshine/app/activity/fragment/MainActivityFragment.java
+++ app/src/main/java/me/cristiangomez/sunshine/app/activity/fragment/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import me.cristiangomez.sunshine.R;
 
@@ -12,6 +16,7 @@ import me.cristiangomez.sunshine.R;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private ListView mForecastListView;
 
     public MainActivityFragment() {
     }
@@ -19,6 +24,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        initializeView(view);
+        return view;
+    }
+
+    public void initializeView(View view) {
+        ArrayList<String> forecasts = new ArrayList<>();
+        forecasts.add("Today - Sunny - 33 / 27");
+        forecasts.add("Tomorrow - Foggy - 33 / 27");
+        forecasts.add("Weds - Cloudy - 33 / 27");
+        forecasts.add("Thurds - Rainy - 33 / 27");
+        forecasts.add("Frid - Foggy - 33 / 27");
+        forecasts.add("Sat - Sunny - 33 / 27");
+        mForecastListView = (ListView) view.findViewById(R.id.listview_forecast);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                forecasts);
+        mForecastListView.setAdapter(arrayAdapter);
     }
 }
